b9c9daad2a6f47a8eb001eac27952bab58d9592a
NekoMashiro <879284464@qq.com>
2018-11-15 23:39:15 +0800

    qtSQL

---
+++ 16_SQL/widget.cpp
@@ -0,0 +1,22 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QSqlDatabase>
+#include <QDebug>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    // 打印Qt支持的数据库驱动
+    qDebug() << QSqlDatabase::drivers();
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName()
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
